af360290bde60fe43f5c9011265e3b1855e85c44
Zion <zion09477@gmail.com>
2026-02-21 23:15:20 +0100

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,19 @@
+function logout() {
+    localStorage.removeItem('user');
+    window.location.href = '../index.html';
+}
+
+function formatCurrency(amount) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(amount);
+}
+
+function formatDate(dateString) {
+    return new Date(dateString).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+    });
+}
